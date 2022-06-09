6ae07e113e303bcc1467554ace04fbb447343f4f
ChaosMo <chenkun03171317@gmail.com>
2022-06-09 20:30:49 +0800

    完成入门mySQL，JDBC

--- hsp30java/src/HelloWorld.java
+++ hsp30java/src/HelloWorld.java
@@ -1,31 +1,47 @@
+import org.junit.jupiter.api.Test;
+
 /**
  * @author [author]
  * @version [version-text]
  */
 public class HelloWorld {
 
-	public static void main(String[] args) {
-		System.out.println("helllo world");
-	}
+    public static void main(String[] args) {
+        System.out.println("helllo world");
+    }
 }
 
-class StringToBasicDetail{
+class StringToBasicDetail {
 
-	public static void main(String[] args) {
-		String str1 = "123"; //转成 int
-		int n1 = Integer.parseInt(str1);
-		System.out.println(n1);
-	}
+    public static void main(String[] args) {
+        String str1 = "123"; //转成 int
+        int n1 = Integer.parseInt(str1);
+        System.out.println(n1);
+    }
 }
 
 class TernaryOperator {
-	
-	public static void main(String[] args) {
-		int a = 10;
-		int b = 99;
-		int result = a > b ? a++ : b--;
-		System.out.println("result=" + result);
-		System.out.println("a=" + a);
-		System.out.println("b=" + b);
-	}
+    public String testName;
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public void setTestName(String testName) {
+        this.testName = testName;
+    }
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 99;
+        int result = a > b ? a++ : b--;
+        System.out.println("result=" + result);
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
+    }
+
+    @Test
+    public void foo(){
+
+    }
 }
